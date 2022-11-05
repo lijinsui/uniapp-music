9d073c5438ffaa21b82b600529f39e3c13b77954
李金穗 <2649113229@qq.com>
2022-11-05 22:28:13 +0800

    tabbar显示

---
+++ pages/index/index.vue
@@ -0,0 +1,59 @@
+<template>
+	<view>
+		<!-- 轮播 -->
+		<view class="banner">
+			<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+				<swiper-item>
+					<view class="swiper-item">aa</view>
+				</swiper-item>
+				<swiper-item>
+					<view class="swiper-item">nn</view>
+				</swiper-item>
+			</swiper>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
